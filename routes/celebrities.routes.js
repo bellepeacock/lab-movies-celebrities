8515eeb32ce153e6514469041a03e4e17474e652
Annabelle Peacock <aspeacock92@gmail.com>
2022-02-16 18:40:47 +0100

    iteration 2 done so far

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Celebrity = require('../models.Celebrity.model')
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+const celebrities = require('../routes/celebrities.routes');
+const movies = require('../routes/movies.routes');
+
 module.exports = router;
